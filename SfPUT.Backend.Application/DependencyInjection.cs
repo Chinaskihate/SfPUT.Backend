7ed222acbc1908836c3e5fd834b33a81a2bf1b76
Chinaskihate <islam.kadyrov.dlschool@gmail.com>
2022-04-28 20:40:04 +0300

    added di

---
+++ SfPUT.Backend.Application/DependencyInjection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using SfPUT.Backend.Application.Interfaces.Comments;
+using SfPUT.Backend.Application.Interfaces.Likes;
+using SfPUT.Backend.Application.Interfaces.Posts;
+using SfPUT.Backend.Application.Interfaces.Rates;
+using SfPUT.Backend.Application.Interfaces.Sections;
+using SfPUT.Backend.Application.Interfaces.Tags;
+using SfPUT.Backend.Application.Interfaces.Users;
+using SfPUT.Backend.Application.Services.Comments;
+using SfPUT.Backend.Application.Services.Likes;
+using SfPUT.Backend.Application.Services.Posts;
+using SfPUT.Backend.Application.Services.Rates;
+using SfPUT.Backend.Application.Services.Sections;
+using SfPUT.Backend.Application.Services.Tags;
+
+namespace SfPUT.Backend.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<ICommentService, CommentService>();
+            services.AddScoped<ILikeService, LikeService>();
+            services.AddScoped<IPostService, PostService>();
+            services.AddScoped<IRateService, RateService>();
+            services.AddScoped<ISectionService, SectionService>();
+            services.AddScoped<ITagService, TagService>();
+            services.AddScoped<IUserService, IUserService>();
+
+            return services;
+        }
+    }
+}
---
+++ SfPUT.Backend.Persistence/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SfPUT.Backend.Application.Interfaces;
+using SfPUT.Backend.Application.Interfaces.DataServices;
+using SfPUT.Backend.Persistence.DataServices;
+
+namespace SfPUT.Backend.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<ICommentDataService, CommentDataService>();
+            services.AddScoped<ILikeDataService, LikeDataService>();
+            services.AddScoped<IPostDataService, PostDataService>();
+            services.AddScoped<IProposedSectionDataService, ProposedSectionDataService>();
+            services.AddScoped<IRateDataService, RateDataService>();
+            services.AddScoped<ISectionDataService, SectionDataService>();
+            services.AddScoped<ITagDataService, TagDataService>();
+
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+                options.EnableSensitiveDataLogging();
+            });
+            services.AddScoped<IAppDbContext>(provider => provider.GetService<AppDbContext>());
+            return services;
+        }
+    }
+}
